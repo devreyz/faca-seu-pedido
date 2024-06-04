04cd0cef947eb47bc42b2d244aa7fb644b50c0cd
reyzin <devreyz137@gmail.com>
2024-06-03 21:58:53 -0300

    Finalizado front-end base

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
     extend: {
+      fontFamily:{
+        "sans": ["Roboto", "sans-serif"]
+      },
       backgroundImage: {
         "home": "url('/assets/img/bg.png')"
       }
